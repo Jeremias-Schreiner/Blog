cd89f29fa027290847b45ff98ae5c59869353e71
Jeremías Schreiner <jeremiasush666@gmail.com>
2025-01-27 14:38:54 -0300

    Creacion de endpoints de la API

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from .views import PostListCreate, PostDetail
+
+urlpatterns = [
+    path("post/", PostListCreate.as_view(), name="post-list-create"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="post-detail"),
+]
